7325356bb7dafdb0a3fbdbbc29baedb876e957e3
Oleksandr <alexandrenko3006@gmail.com>
2025-03-03 17:39:15 -0500

    Added logger with handlers

--- task2.py
+++ task2.py
@@ -1,45 +1,65 @@
 from abc import ABC, abstractmethod
+import logging
+from typing import List
 
+logger = logging.getLogger("LibraryLogger")
+logger.setLevel(logging.INFO)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+
+file_handler = logging.FileHandler("library.log", mode="a")
+file_handler.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
 
 class Book:
-    def __init__(self, title, author, year):
+    def __init__(self, title: str, author: str, year: int):
         self.title = title
         self.author = author
         self.year = year
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"Title: {self.title}, Author: {self.author}, Year: {self.year}"
 
 
 class LibraryInterface(ABC):
     @abstractmethod
-    def add_book(self, book: Book):
+    def add_book(self, book: Book) -> None:
         pass
 
     @abstractmethod
-    def remove_book(self, title: str):
+    def remove_book(self, title: str) -> None:
         pass
 
     @abstractmethod
-    def show_books(self):
+    def show_books(self) -> None:
         pass
 
 
 class Library(LibraryInterface):
     def __init__(self):
-        self.books = []
+        self.books: List[Book] = []
 
-    def add_book(self, book: Book):
-        return self.books.append(book)
-    
-    def remove_book(self, title):
+    def add_book(self, book: Book) -> None:
+        self.books.append(book)
+        logger.info(f"Added book: {book}")
+
+    def remove_book(self, title: str) -> None:
         self.books = [book for book in self.books if book.title != title]
+        logger.info(f"Removed book: {title}")
 
-    def show_books(self):
+    def show_books(self) -> None:
         if not self.books:
-            print("Library is empty")
+            logger.info("Library is empty")
         for book in self.books:
-            print(book)
+            logger.info(book)
 
 
 class LibraryManager:
